4524f1102d8ec4ff81c3fe69fa9081ddd934140d
Mr-Pantex <62232794+Mr-Pantex@users.noreply.github.com>
2020-05-26 02:30:13 -0100

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -19,8 +19,8 @@ logo = """
 / __| '_ \ / _` | '_ ` _ \| '_ ` _ \ / _ \ '__|
 \__ \ |_) | (_| | | | | | | | | | | |  __/ |
 |___/ .__/ \__,_|_| |_| |_|_| |_| |_|\___|_|
-    | | %sAuthor %s: %sRizky ID
-%s    |_| %sGithub %s: %shttps://github.com/hekelpro
+    | | %sAuthor %s: %sMr-Pantex
+%s    |_| %sGithub %s: %shttps://github.com/Mr-pantex
 """%(cyan,hijau,merah,putih,cyan,hijau,merah,putih)
 
 def main():
